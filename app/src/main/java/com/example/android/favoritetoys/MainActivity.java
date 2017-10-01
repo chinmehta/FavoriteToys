9d2c8b5150e9ef442bdff74c3c81b8ab76cce44e
Chintan Mehta <mchintan24@gmail.com>
2017-10-01 20:13:34 +0530

    Exercise 3

--- app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -21,19 +21,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mToysListTextView;
+
+    private TextView mToysListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
 
+
         String[] toyNames = ToyBox.getToyNames();
-        
-        for(String toyName : toyNames)
-        {
+
+
+        for (String toyName : toyNames) {
             mToysListTextView.append(toyName + "\n\n\n");
         }
     }
